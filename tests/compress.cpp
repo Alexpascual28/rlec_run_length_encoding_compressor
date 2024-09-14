1771fa813dfc42f51a7fa945d1aba99f2a690b17
Alexpascual28 <apsr2809@gmail.com>
2024-09-14 18:34:55 +0100

    cli messages and test 5 added to compress and decompress files

--- tests/compress.cpp
+++ tests/compress.cpp
@@ -1,14 +1,21 @@
 #include "file_compression_system.h"
 #include <iostream>
 
-void testCompress(const std::string& input, const std::string& expectedOutput) {
+void testCompress(int testNumber, const std::string& input, const std::string& expectedOutput) {
+    std::cout << "Test " << testNumber << std::endl;
+
+    std::cout << "Testing compression of: " << input << std::endl;
+    std::cout << "Expected output: " << expectedOutput << std::endl;
+
     Compressor compressor;
     std::string output = compressor.compress(input);
+    std::cout << "Actual output: " << output << std::endl;
+
     if (output == expectedOutput) {
-        std::cout << "Test passed!" << std::endl;
+        std::cout << "Test passed!\n" << std::endl;
     }
     else {
-        std::cout << "Test failed!" << std::endl;
+        std::cout << "Test failed!\n" << std::endl;
     }
 }
 
@@ -17,12 +24,14 @@ int main(int argc, char *argv[]) {
     const std::string expectedOutputTest2 = "abcd";
     const std::string expectedOutputTest3 = "";
     const std::string expectedOutputTest4 = "158743";
+    const std::string expectedOutputTest5 = "&8@9$%3";
 
     Compressor compressor;
 
-    testCompress("aaabbcc", expectedOutputTest1);
-    testCompress("abcd", expectedOutputTest2);
-    testCompress("", expectedOutputTest3);
-    testCompress("111118888888444", expectedOutputTest4);
+    testCompress(1, "aaabbcc", expectedOutputTest1);
+    testCompress(2, "abcd", expectedOutputTest2);
+    testCompress(3, "", expectedOutputTest3);
+    testCompress(4, "111118888888444", expectedOutputTest4);
+    testCompress(5, "&&&&&&&&@@@@@@@@@$%%%", expectedOutputTest5);
 
 }
--- tests/decompress.cpp
+++ tests/decompress.cpp
@@ -1,14 +1,21 @@
 #include "file_compression_system.h"
 #include <iostream>
 
-void testDecompress(const std::string& input, const std::string& expectedOutput) {
+void testDecompress(int testNumber, const std::string& input, const std::string& expectedOutput) {
+    std::cout << "Test " << testNumber << std::endl;
+
+    std::cout << "Testing decompression of: " << input << std::endl;
+    std::cout << "Expected output: " << expectedOutput << std::endl;
+
     Compressor compressor;
     std::string output = compressor.decompress(input);
+    std::cout << "Actual output: " << output << std::endl;
+
     if (output == expectedOutput) {
-        std::cout << "Test passed!" << std::endl;
+        std::cout << "Test passed!\n" << std::endl;
     }
     else {
-        std::cout << "Test failed!" << std::endl;
+        std::cout << "Test failed!\n" << std::endl;
     }
 }
 
@@ -17,12 +24,14 @@ int main(int argc, char *argv[]) {
     const std::string expectedOutputTest2 = "abcd";
     const std::string expectedOutputTest3 = "";
     const std::string expectedOutputTest4 = "111118888888444";
+    const std::string expectedOutputTest5 = "&&&&&&&&@@@@@@@@@$%%%";
 
     Compressor compressor;
 
-    testDecompress("a3b2c2", expectedOutputTest1);
-    testDecompress("abcd", expectedOutputTest2);
-    testDecompress("", expectedOutputTest3);
-    testDecompress("158743", expectedOutputTest4);
+    testDecompress(1, "a3b2c2", expectedOutputTest1);
+    testDecompress(2, "abcd", expectedOutputTest2);
+    testDecompress(3, "", expectedOutputTest3);
+    testDecompress(4, "158743", expectedOutputTest4);
+    testDecompress(5, "&8@9$%3", expectedOutputTest5);
 
 }
